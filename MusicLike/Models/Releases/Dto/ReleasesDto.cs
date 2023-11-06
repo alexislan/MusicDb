6b62d1929e413d87699b36831537929060800b75
alexislan <alexislanchiotti13@gmail.com>
2023-11-06 12:29:24 -0300

    "creacion del proy"

---
+++ MusicLike/Models/Releases/Dto/ReleasesDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicLike.Models.Releases.Dto
+{
+    public class ReleasesDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public Decimal Score { get; set; }
+        public int Ratings { get; set; }
+    }
+}
---
+++ MusicLike/Models/Releases/Releases.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLike.Models.Releases
+{
+    public class Releases
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public int Score { get; set; }
+        public int Ratings { get; set; }
+        public List<Genres.Genres> Genres { get; set;}
+        [Required]
+        public int ReleaseTypeId { get; set; }
+        [ForeignKey("ReleaseTypeId")]
+        public ReleaseType.ReleaseType ReleaseType { get; set; }
+    }
+
+    public class ReleaseArtist
+    {
+        public int ArtistId { get; set; }
+        public int ReleaseId { get; set; }
+    }
+}
