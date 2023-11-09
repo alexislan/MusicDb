4515588fb3a5b92e963735da41e9951ab41c671c
alexislan <alexislanchiotti13@gmail.com>
2023-11-09 01:01:34 -0300

    backend terminado solo faltan detalles de codigo

---
+++ MusicLike/Models/Releases/Dto/ReleaseGetDto.cs
@@ -0,0 +1,28 @@
+﻿using MusicLike.Models.Artists.Dto;
+using MusicLike.Models.Genres.GenresDto;
+using MusicLike.Models.Rating.Dto;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicLike.Models.Releases.Dto
+{
+    public class ReleaseGetDto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [Range(typeof(DateTime), "2000-01-01", "2100-12-31")]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public int Score { get; set; }
+        public int RatingId { get; set; }
+        public ArtistDto Artist { get; set; }
+        public GenresDto Genre { get; set; }
+        public RatingDto Rating { get; set; }
+
+    }
+}
